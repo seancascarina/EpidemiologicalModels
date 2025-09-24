6a492535a63ba952de26d6a39644541cc5c728e1
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-09-24 07:59:02 -0600

    Add get_results_files() to parse filenames from batch file

--- make_SIRmodel_Animation.py
+++ make_SIRmodel_Animation.py
@@ -1,7 +1,26 @@
 
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
 def main():
+    
+    batch_file = 'RUN_SIRmodel_RecoveryTime-varied.bat'
+    files = get_results_files(batch_file)
 
+
+def get_results_files(batch_file):
+    
+    files = []
+    h = open(batch_file)
+    for line in h:
+        start, end = line.rstrip().split('-o ')
+        results_file, *_ = end.split(' ')
+        files.append(results_file)
+        
+    h.close()
+    
+    return files
+    
 
 if __name__ == '__main__':
     main()
