6b553cb3bcc4cb8cdcf2e8de9caadcdfc5c07e93
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-09-27 14:53:14 -0600

    Add second y axis for reproduction number

--- make_SIRmodel_Animation_withEffectiveReproductionNumber_RecoveryRateVaried.py
+++ make_SIRmodel_Animation_withEffectiveReproductionNumber_RecoveryRateVaried.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import seaborn as sns
-categories = ['Susceptible', 'Infected', 'Recovered']
+categories = ['Susceptible', 'Infected', 'Recovered', 'Effective Reproduction Number']
 color_palette = sns.color_palette()[:len(categories)]
 
 
@@ -18,13 +18,18 @@ def animate(i, df, param_values, num_interpolations):
     
     y = df[categories[2]][i]
     line3.set_data(x, y)
+    
+    y = df[categories[3]][i]
+    line4.set_data(x, y)
+    
+    ax2.set_ylim(min(y), max(y))
 
     if i % num_interpolations == 0:
         index = int(i / num_interpolations)
         value = param_values[index]
         title.set_text(f"Recovery Time: {value} days")
     
-    return line1, line2, line3, title
+    return line1, line2, line3, line4, title
     
     
 def calc_interpolations(df, num_interpolations):
@@ -51,8 +56,9 @@ def init():
     line1.set_data([], [])
     line2.set_data([], [])
     line3.set_data([], [])
+    line4.set_data([], [])
 
-    return line1, line2, line3
+    return line1, line2, line3, line4
     
 
 def get_data(file, df, params):
@@ -92,9 +98,9 @@ def get_results_files(batch_file):
     h.close()
     
     return files
+
     
-    
-batch_file = 'RUN_SIRmodel_RecoveryTime-varied.bat'
+batch_file = 'RUN_SIRmodel_with_EffectiveReproductionNumber_RecoveryTime-varied.bat'
 param_values = list(np.linspace(3, 12, num=19))
 initial_title = f"Recovery Time: {param_values.pop(0)} days"
 files = get_results_files(batch_file)
@@ -114,6 +120,7 @@ days = int(params[-1])
 # INITIALIZE THE FIGURE
 fig = plt.figure()
 ax = plt.axes(xlim=(0, days), ylim=(0, N))
+ax2 = ax.twinx()
 ax.set_xlabel('Day', fontname='Arial', fontsize=14)
 ax.set_ylabel('Number of Individuals\n(per 1000)', fontname='Arial', fontsize=14)
 for tick in ax.get_xticklabels():
@@ -125,6 +132,7 @@ for tick in ax.get_yticklabels():
 line1, = ax.plot([], [], lw=2, color=color_palette[0], label=categories[0])
 line2, = ax.plot([], [], lw=2, color=color_palette[1], label=categories[1])
 line3, = ax.plot([], [], lw=2, color=color_palette[2], label=categories[2])
+line4, = ax2.plot([], [], lw=2, color=color_palette[3], label=categories[3])
 lines = [line1, line2, line3]
 ax.legend(loc=2, bbox_to_anchor=(1,1))
 title = ax.text(0.5, 1.0, initial_title,
